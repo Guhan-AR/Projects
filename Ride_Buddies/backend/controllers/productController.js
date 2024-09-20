de9a6bec6efadd33afafc092d2cf08e0a6c59746
Guhan-AR <168805253+Guhan-AR@users.noreply.github.com>
2024-09-20 14:36:42 +0530

    succefully created 4 api with basic functionalities

--- Ride_Buddies/backend/controllers/productController.js
+++ Ride_Buddies/backend/controllers/productController.js
@@ -1,9 +1,13 @@
 const Product = require('../models/productModel');
 
-exports.getProducts = (req,res,next)=>{
+//Get products - http://localhost:8000/api/v1/products
+exports.getProducts =async (req,res,next)=>{
+    const products = await Product.find();
     res.status(200).json({
         successs : true,
-        message : "This route will show all the products in database"
+        // message : "This route will show all the products in database"
+        count : products.length,
+        products
     })
 }
 
@@ -11,6 +15,42 @@ exports.getProducts = (req,res,next)=>{
 exports.newProduct =async (req,res,next)=>{
     const product = await Product.create(req.body)
     res.status(201).json({
+        success:true,
+        product
+    })
+}
+
+//Get Single Product - http://localhost:8000/api/v1/product/66ead8198c38aa66fb2f57e4
+exports.getSingleProduct = async(req,res,next)=>{
+    const product =await Product.findById(req.params.id);
+
+    if (!product){
+        return res.status(404).json({
+            success: false,
+            message : "Product not found"
+        })
+    }
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
+
+//Update Product - 
+exports.updateProduct = async (req,res,next)=>{
+    let product = await Product.findById(req.params.id)
+    if (!product){
+        return res.status(404).json({
+            success: false,
+            message : "Product not found"
+        })
+    }
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new : true,
+        runValidators : true
+
+    })
+    res.status(200).json({
         success:true,
         product
     })
